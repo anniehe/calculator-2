e1e04e3279338a0bc3f2f50124b2cc80351a8193
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 16:22:02 -0800

    used len() and whole thing runs!

--- calculator.py
+++ calculator.py
@@ -18,25 +18,28 @@ while problem_pieces[0] != "q":
 
     operator = problem_pieces[0]
     try:
-        if problem_pieces[1]:
+        if len(problem_pieces) > 1:
             num1 = int(problem_pieces[1])
-        if problem_pieces[2]:
+        if len(problem_pieces) > 2:
             num2 = int(problem_pieces[2])
     except ValueError:
         print "These are not valid inputs."
     else:
         if operator == "+":
             print add(num1, num2)
-        # elif operator == "-":
-
-        # elif operator == "*":
-
-        # elif operator == "/":
-
-        # elif operator == "square":
-
-        # elif operator == "cube":
-
-        # elif operator == "pow":
-
-        # elif operator == "mod":
+        elif operator == "-":
+            print subtract(num1, num2)
+        elif operator == "*":
+            print multiply(num1, num2)
+        elif operator == "/":
+            print divide(num1, num2)
+        elif operator == "square":
+            print square(num1)
+        elif operator == "cube":
+            print cube(num1)
+        elif operator == "pow":
+            print power(num1, num2)
+        elif operator == "mod":
+            print mod(num1, num2)
+        else:
+            print "That's not a valid operator."
